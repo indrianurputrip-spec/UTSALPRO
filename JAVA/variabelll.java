e6eea528fa1fae72269fa869b5b8b808debeea12
indrianurputrip-spec <indrianurputrip@gmail.com>
2025-11-09 23:18:27 +0700

    java

---
+++ JAVA/variabelll.java
@@ -0,0 +1,28 @@
+package JAVA;
+
+public class variabelll {
+     public static void main(String[] args) {
+
+        // deklarasi dan inisialisasi variabel dengan tipe data yang sesuai 
+        String namaLengkap = "Indria nurputri pratiwi";
+        String tempatLahir = "Sukabumi";
+        String tanggalLahir =  "27-02-2007";
+        String jenisKelamin = "Perempuan";
+        String alamat = "jl.nyangkowek perum setia budi permai blokD3";
+        String nomerTelepon = "082115147201";
+        String penghasilanSebulan = "20000000";
+
+        // menampilkan output 
+        System.out.println("Assalamualaikum wr.wb");
+        System.out.println("Izinkan saya memperkenalkan diri");
+        System.out.println("Nama lengkap\t: " + namaLengkap);
+        System.out.println("Tempat & Tanggal Lahir\t: " + tempatLahir + ", " + tanggalLahir); 
+        System.out.println("Jenis Kelamin\t: " + jenisKelamin);
+        System.out.println("Alamat\t: " + alamat);
+        System.out.println("No. Telepon\t: " + nomerTelepon);
+        System.out.println("Penghasilan Sebulan\t: " + penghasilanSebulan);
+
+        // catatan: \t digunakan untuk tab agar output lebih rapi
+        }
+    }
+
